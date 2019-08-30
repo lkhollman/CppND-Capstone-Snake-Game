4f22ec917504d4feefa281dd2858d3b3e04df21d
lkhollman <lkhollman@yahoo.com>
2019-08-30 13:36:16 -0400

    update comments

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,19 @@
 #include "renderer.h"
 
 int main() {
-  constexpr std::size_t kFramesPerSecond{60};
-  constexpr std::size_t kMsPerFrame{1000 / kFramesPerSecond};
-  constexpr std::size_t kScreenWidth{640};
-  constexpr std::size_t kScreenHeight{640};
-  constexpr std::size_t kGridWidth{32};
-  constexpr std::size_t kGridHeight{32};
+  constexpr std::size_t kFramesPerSecond{60};  //game play frames per second
+  constexpr std::size_t kMsPerFrame{1000 / kFramesPerSecond};  //compute 1000ms per second divided by frames per second
+  constexpr std::size_t kScreenWidth{640};  //width in pixels
+  constexpr std::size_t kScreenHeight{640};   //height in pixels
+  constexpr std::size_t kGridWidth{32};   //grid of 32 cells wide
+  constexpr std::size_t kGridHeight{32};   //by 32 cells high
 
-  Renderer renderer(kScreenWidth, kScreenHeight, kGridWidth, kGridHeight);
-  Controller controller;
-  Game game(kGridWidth, kGridHeight);
-  game.Run(controller, renderer, kMsPerFrame);
-  std::cout << "Game has terminated successfully!\n";
-  std::cout << "Score: " << game.GetScore() << "\n";
-  std::cout << "Size: " << game.GetSize() << "\n";
+  Renderer renderer(kScreenWidth, kScreenHeight, kGridWidth, kGridHeight);  //create renderer object
+  Controller controller; //create controller object
+  Game game(kGridWidth, kGridHeight);  //create game object
+  game.Run(controller, renderer, kMsPerFrame);  //call game.Run to start game loop; and pass in controller, renderer, and Ms/frame
+  std::cout << "Game has terminated successfully!\n";  //print when game is done
+  std::cout << "Score: " << game.GetScore() << "\n";   //print when game is done
+  std::cout << "Size: " << game.GetSize() << "\n";    //print when game is done
   return 0;
 }
